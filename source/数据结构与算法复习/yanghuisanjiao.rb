1283466dc750b0fcf09384f91fa0832a26389742
simlegate <simlegate@163.com>
2013-07-15 23:13:10 +0800

    修改ruby杨辉三角代码

--- source/数据结构与算法复习/yanghuisanjiao.rb
+++ source/数据结构与算法复习/yanghuisanjiao.rb
@@ -6,17 +6,16 @@ class Sanjiao
   end
 
   def sanjiaoxing(temp)
+    # 打印出本行元素
     temp.each{ |t| print t.to_s + ' '}
     array = Array.new(temp.length + 1)
     array_max_index = array.length - 1
 
-    # 组建下一行，即下一行数组
+    # 组建下一行，即下一行数组array
     0.upto(array_max_index).each do |a|
       # 两端的就不用求
-      if a == 0
-	array[0] = 1
-      elsif a == array_max_index
-	array[array_max_index] = 1
+      if a == 0 || a == array_max_index
+	array[a] = 1
       else
         # 根据上一个数组的结果得下一数组的值
 	# to_i确保前一个数为nil时转换为0
