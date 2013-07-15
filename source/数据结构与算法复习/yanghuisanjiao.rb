e9cefdc92f35e7d7fa1b6521a912047ca63672fb
simlegate <simlegate@163.com>
2013-07-15 23:06:49 +0800

    实现杨辉三角

---
+++ source/数据结构与算法复习/yanghuisanjiao.rb
@@ -0,0 +1,36 @@
+# 递归思想每一行我看做是一个数组，传入的参数是上一个数组
+# 当然每一个结果数组用temp缓存一下，每传入一个数组，那么打印一次，也就是每行的结果
+class Sanjiao
+  def initialize row
+    @row = row
+  end
+
+  def sanjiaoxing(temp)
+    temp.each{ |t| print t.to_s + ' '}
+    array = Array.new(temp.length + 1)
+    array_max_index = array.length - 1
+
+    # 组建下一行，即下一行数组
+    0.upto(array_max_index).each do |a|
+      # 两端的就不用求
+      if a == 0
+	array[0] = 1
+      elsif a == array_max_index
+	array[array_max_index] = 1
+      else
+        # 根据上一个数组的结果得下一数组的值
+	# to_i确保前一个数为nil时转换为0
+	array[a] = temp[a-1].to_i + temp[a].to_i
+      end
+    end
+
+    puts ''
+    sanjiaoxing(array) if array.length < @row
+  end
+end
+
+# 初始化temp的长度为1
+a = Array.new(1)
+a[0] = 1
+s = Sanjiao.new(20)
+s.sanjiaoxing(a)
