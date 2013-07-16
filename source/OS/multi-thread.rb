c936ee730589a83342215a2fa463919e14b1aacc
simlegate <simlegate@163.com>
2013-07-16 19:03:30 +0800

    ruby多线程 #1

---
+++ source/OS/multi-thread.rb
@@ -0,0 +1,24 @@
+def thread_1
+  i = 0
+  while i <= 2
+    puts  "Thread 1 At #{Time.now}"
+    sleep(2)
+    i = i + 1
+  end
+end
+
+def thread_2
+  j = 0
+  while j <= 2
+    puts  "Thread 2 At #{Time.now}"
+    sleep(2)
+    j = j + 1
+  end
+end
+
+puts "Started At #{Time.now}"
+t1 = Thread.new {thread_1}
+t2 = Thread.new {thread_2}
+t1.join
+t2.join
+puts "End At #{Time.now}"
